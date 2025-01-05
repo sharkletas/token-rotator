8a1d96fee95d5a42fccfa856a3d7c1d8077fc129
joseph.mattey <joseph.mattey@First-Factory-Inc-MacBookPro-VG2XWRTYY3.local>
2025-01-04 21:26:47 -0600

    Actualización de Variable de Entorno en Vercel removida, variable de entorno en Render actualizada.

--- tokenGenRot.sh
+++ tokenGenRot.sh
@@ -22,30 +22,6 @@ update_render_env_var() {
              ]'
 }
 
-# Función para actualizar la variable de entorno en Vercel
-update_vercel_env_var() {
-    local project_id=$1
-    node -e "const axios = require('axios');
-    axios.patch('https://api.vercel.com/v12/projects/${project_id}/env',
-        {
-            type: 'encrypted',
-            key: 'API_TOKEN',
-            value: '${NEW_TOKEN}',
-            target: ['production']
-        },
-        {
-            headers: {
-                'Authorization': 'Bearer ${VERCEL_API_KEY}',
-                'Content-Type': 'application/json'
-            }
-        }
-    ).then(response => {
-        console.log('Vercel environment variable updated:', response.data);
-    }).catch(error => {
-        console.error('Failed to update Vercel environment variable:', error.message);
-    });"
-}
-
 # Actualizar la variable de entorno en la instancia principal de Render
 echo "Actualizando API_TOKEN en la instancia principal de Render..."
 update_render_env_var "$RENDER_SERVICE_ID"
@@ -53,7 +29,3 @@ update_render_env_var "$RENDER_SERVICE_ID"
 # Actualizar la variable de entorno en la instancia del cron job en Render
 echo "Actualizando API_TOKEN en la instancia del cron job en Render..."
 update_render_env_var "$RENDER_CRON_JOB_SERVICE_ID"
-
-# Actualizar la variable de entorno en Vercel
-echo "Actualizando API_TOKEN en Vercel..."
-update_vercel_env_var "your-vercel-project-id" # Reemplaza con el ID real de tu proyecto en Vercel
